ad5a002458b17ddc95e03b9726b3786dbee86302
leeyeonju02 <dlduswn0783@naver.com>
2024-09-08 19:47:44 +0900

    first commit

---
+++ src/main/java/com/example/mini/please_mini_pjt/PleaseMiniPjtApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini.please_mini_pjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PleaseMiniPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PleaseMiniPjtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mini/please_mini_pjt/PleaseMiniPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini.please_mini_pjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PleaseMiniPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
